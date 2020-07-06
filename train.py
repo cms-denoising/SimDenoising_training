2c2de5504cf6004ecd31823f9becf7e3b7131ea4
Lena Franklin <lenafranklin555@gmail.com>
2020-07-06 10:57:51 -0500

    add epochs argument to training

--- train.py
+++ train.py
@@ -22,7 +22,9 @@ parser.add_argument("validation_path", nargs="?", type=str, default="./data/vali
 parser.add_argument("--num_of_layers", type=int, default=9, help="Number of total layers")
 parser.add_argument("--sigma", type=float, default=25, help='noise level')
 parser.add_argument("--outf", type=str, default="logs", help='path of log files')
-opt = parser.parse_args()
+parser.add_argument("--epochs", type=int, default=2, help="Number of training epochs")
+parser.add_argument("--lr", type=float, default=1e-3, help="Initial learning rate")
+args = parser.parse_args()
 
 def init_weights(m):
     if type(m) == nn.Linear:
@@ -30,63 +32,83 @@ def init_weights(m):
         m.bias.data.fill_(0.01)
 
 def main():
-    model = DnCNN(channels=1, num_of_layers=opt.num_of_layers)
+    args.device = torch.device('cpu')
+    # check for gpu
+    if torch.cuda.is_available():
+        args.device = torch.device('cuda')
+        print("Switched to gpu")
+    else:
+        print("Using CPU")
+    model = DnCNN(channels=1, num_of_layers=args.num_of_layers).to(device=args.device)
     model.apply(init_weights)
     criterion = PatchLoss()
-    optimizer = optim.Adam(model.parameters(), lr = 0.001)
-    writer = SummaryWriter(opt.outf)
+    criterion.to(device=args.device)
+    optimizer = optim.Adam(model.parameters(), lr = args.lr)
+    writer = SummaryWriter(args.outf)
+    # numpy arrays for storing stuff.. this is a Very Inefficient Method for doing this but
+    # hopefully its just temporary until I get enough space to install tensorboard...
+    train_loss = np.empty(0)
+    train_maxdiff = np.empty(0)
+    validate_loss = np.empty(0)
+    validate_maxdiff = np.empty(0)
+    train_step_number = np.empty(0)
+    validate_step_number = np.empty(0)
     # train the net
-    training_files = glob.glob(os.path.join(opt.training_path, '*.root'))
-    for training_file in training_files:
-        print("Opened file " + training_file)
-        branch = get_all_histograms(training_file)
-        length = np.size(branch)
-        for i in range(1):
-            print("Begin processing event " + str(i) + " of " + str(length) + " in file") 
-            model.train()
-            model.zero_grad()
-            optimizer.zero_grad()
-            # get data (ground truth)
-            data = get_bin_weights(branch, 0).copy()
-            # add noise
-            noisy = add_noise(data, opt.sigma).copy()
-            # convert to tensor
-            data = torch.from_numpy(data)
-            noisy = torch.from_numpy(noisy)
-            noisy = noisy.unsqueeze(0)
-            noisy = noisy.unsqueeze(1)
-            out_train = model(noisy.float())
-            loss = criterion(out_train.squeeze(0).squeeze(0), data, 10)
-            loss.backward()
-            optimizer.step()
+    step = 0
+    for epoch in range(args.epochs):
+        training_files = glob.glob(os.path.join(args.training_path, '*.root'))
+        for training_file in training_files:
+            print("Opened file " + training_file)
+            branch = get_all_histograms(training_file)
+            length = np.size(branch)
+            for i in range(length):
+                model.train()
+                model.zero_grad()
+                optimizer.zero_grad()
+                # get data (ground truth)
+                data = get_bin_weights(branch, 0).copy()
+                # add noise
+                noisy = add_noise(data, args.sigma).copy()
+                # convert to tensor
+                data = torch.from_numpy(data).to(device=args.device)
+                noisy = torch.from_numpy(noisy)
+                noisy = noisy.unsqueeze(0)
+                noisy = noisy.unsqueeze(1).to(device=args.device)
+                out_train = model(noisy.float()).to(device=args.device)
+                loss = criterion(out_train.squeeze(0).squeeze(0), data, 10)
+                loss.backward()
+                optimizer.step()
+                model.eval()
+                if step % 10 ==0:
+                    train_loss = np.append(train_loss, loss.item())
+                step+=1
             model.eval()
-        model.eval()
-    # validation
-    validation_files = glob.glob(os.path.join(opt.validation_path, '*root'))
-    # peak signal to noise ratio
-    loss_val = 0
-    count = 0
-    for validation_file in validation_files:
-        print("Opened file " + validation_file)
-        branch = get_all_histograms(validation_file)
-        length = np.size(branch)
-        for i in range (1):
-            # get data (ground truth)
-            data = get_bin_weights(branch, 0).copy()
-            # add noise
-            noisy = add_noise(data, opt.sigma).copy()
-            # convert to tensor
-            data = torch.from_numpy(data)
-            noisy = torch.from_numpy(noisy)
-            noisy = noisy.unsqueeze(0)
-            noisy = noisy.unsqueeze(1)
-            out_train = model(noisy.float())
-            loss_val+=criterion(out_train.squeeze(0).squeeze(0), data, 10)
-        loss_val/=length
-        #writer.add_scalar('Loss values on validation data', loss_val, count)
-        count+=1
-    # save the model
-    torch.save(model.state_dict(), os.path.join(opt.outf, 'net.pth'))
+        # validation
+        validation_files = glob.glob(os.path.join(args.validation_path, '*root'))
+        # peak signal to noise ratio
+        loss_val = 0
+        count = 0
+        for validation_file in validation_files:
+            print("Opened file " + validation_file)
+            branch = get_all_histograms(validation_file)
+            length = np.size(branch)
+            for i in range (length):
+                # get data (ground truth)
+                data = get_bin_weights(branch, 0).copy()
+                # add noise
+                noisy = add_noise(data, opt.sigma).copy()
+                # convert to tensor
+                data = torch.from_numpy(data).to(device=args.device)
+                noisy = torch.from_numpy(noisy)
+                noisy = noisy.unsqueeze(0)
+                noisy = noisy.unsqueeze(1).to(device=args.device)
+                out_train = model(noisy.float()).to(device=args.device)
+                loss_val+=criterion(out_train.squeeze(0).squeeze(0), data, 10)
+            loss_val/=length
+            writer.add_scalar('Loss values on validation data', loss_val, count)
+            count+=1
+        # save the model
+        torch.save(model.state_dict(), os.path.join(args.outf, 'net.pth'))
         
 if __name__ == "__main__":
     main()
