d632c19751a4f8df08fad2862dc592292209d87d
ÐÐÑTara Leininger <leiningt@fnal.gov>
2021-07-28 08:51:44 -0500

    new class added to dataset.py

--- dataset.py
+++ dataset.py
@@ -7,16 +7,6 @@ import torch.utils.data as udata
 import torch 
 import math
 
-def get_branch(file_paths):
-    file_path = file_paths.split(' ')
-    branch = []
-    for i, elem in enumerate(file_path):
-        file = up.open(file_path[i])
-        tree = file["g4SimHits/tree"]
-        events = tree["bin_weights"].array()
-        branch = np.concatenate((branch, events))
-    return branch;
-
 def get_flips():
     flipx = random.randint(0, 1)
     flipy = random.randint(0, 1)
@@ -24,7 +14,7 @@ def get_flips():
     return flipx, flipy, rot
     
 
-def get_bin_weights(branch, n, flipx, flipy, rot):
+def get_bin_weights(branch, n, flipx = None, flipy=None, rot=None):
     data = np.zeros((100,100))
     count = 0
     for y in range(100):
@@ -38,32 +28,65 @@ def get_bin_weights(branch, n, flipx, flipy, rot):
         data = np.fliplr(data)
     if flipy:
         data = np.flipud(data)
-    for i in range(rot):
-        data = np.rot90(data)
+    if rot:
+        for i in range(rot):
+            data = np.rot90(data)
     return data;
     
 
 def add_noise(data, sigma):
     return np.clip(data + np.random.normal(loc=0.0,scale=sigma, size=[100,100]), a_min=0, a_max=None);
 
-class RootDataset(udata.Dataset):
+
+def get_tree(file_path):
+    file = up.open(file_path)
+    tree = file["g4SimHits/tree"]
+    return tree;
+
+def get_branch(file_paths):
+    file_path = file_paths.split(' ')
+    branch = []
+    for i, elem in enumerate(file_path):
+        tree = get_tree(file_path[i])
+        events = tree["bin_weights"].array()
+        branch = np.concatenate((branch, events))
+    return branch;
+
+
+#check that all functions called in RootBasic are added to dataset.py
+class RootBasic(udata.Dataset):
     allowed_transforms = ["none","normalize","log10"]
     def __init__(self, fuzzy_root, sharp_root, transform='none'):
         self.sharp_root = sharp_root
         self.fuzzy_root = fuzzy_root
-        #self.sigma = sigma
         self.sharp_branch = get_branch(sharp_root)
         self.fuzzy_branch = get_branch(fuzzy_root)
+        self.sharp_tree = get_tree(self.sharp_root)
+        self.fuzzy_tree = get_tree(self.fuzzy_root)
         self.transform = transform
+        self.means = None
+        self.stdevs = None
         if self.transform not in self.allowed_transforms:
             raise ValueError("Unknown transform: {}".format(self.transform))
-        
+
     def __len__(self):
         if len(self.sharp_branch) == len(self.fuzzy_branch):
             return len(self.sharp_branch)
         else:
             print("Sharp and fuzzy dataset lengths do not match")
+            
+    def __getitem__(self, idx):
+        sharp_np = self.sharp_branch.copy()[idx]
+        fuzzy_np = self.fuzzy_branch.copy()[idx]
+        return sharp_np, fuzzy_np
+    
+    #can only be called after __getitem__ has run in RootDataset
+    def unnormalize(self,array):
+        array *= self.stdevs
+        array += self.means 
+        return array
 
+class RootDataset(RootBasic):
     def __getitem__(self, idx):
         flipx, flipy, rot = get_flips()
         sharp_np = get_bin_weights(self.sharp_branch, idx, flipx, flipy, rot).copy()
@@ -73,17 +96,26 @@ class RootDataset(udata.Dataset):
             sharp_np = np.log10(sharp_np, where=sharp_np>0)
             fuzzy_np = np.log10(fuzzy_np, where=fuzzy_np>0)
         elif self.transform=="normalize":
-            means = np.mean(sharp_np)
-            stdevs = np.std(sharp_np)
-            sharp_np -= means
-            sharp_np /= stdevs
-            fuzzy_np -= means
-            fuzzy_np /= stdevs
+            self.means = np.mean(sharp_np)
+            self.stdevs = np.std(sharp_np)
+            sharp_np -= self.means
+            sharp_np /= self.stdevs
+            fuzzy_np -= self.means
+            fuzzy_np /= self.stdevs
         
         sharp = torch.from_numpy(sharp_np)
         fuzzy = torch.from_numpy(fuzzy_np)
         return sharp, fuzzy 
-
+    
+    
+    def getMean(self, event):
+        mean = np.mean(get_bin_weights(self.sharp_branch, event).copy())
+        return mean
+    
+    def getStdev(self, event):
+        stdev = np.std(get_bin_weights(self.sharp_branch, event).copy())
+        return stdev
+        
 if __name__=="__main__":
     dataset = RootDataset("test.root", 1)
     truth, noise = dataset.__getitem__(0) 
